f640746401bb46f4ecc73a691174f663cb0cbbfb
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-08-22 13:23:10 +0000

    Fix bug

--- main.py
+++ main.py
@@ -14,10 +14,18 @@ def render():
 def receive() :
     if request.method == "POST" :
         f = request.files['file']
-        img_req = Image.open(f).copy()
+        img_req = Image.open(f).copy().convert("RGBA")
         img_twibbon = Image.open("./static/ImagesTwibbon/twibbon1.png").copy().convert("RGBA")
+        img_new = Image.new("RGBA", (img_twibbon.width, img_twibbon.height)).copy()
 
-        img_data = img_twibbon.getdata()
+        resize = img_req.resize((int(img_new.width), int(img_new.height)))
+        
+        img_new.paste(resize, (0, 0), resize)
+        img_new.paste(img_twibbon, (0, 0), img_twibbon)
+
+        
+        
+        img_twibbon.getdata()
 
         datas = []
 
@@ -28,12 +36,13 @@ def receive() :
             datas.append(item)
 
         img_twibbon.putdata(datas)
-        img_twibbon.thumbnail((img_req.width, img_req.height))
-        img_req.paste(img_twibbon, (0, 0))
+        
+        
         
         bytes = io.BytesIO()
-        img_req.save(bytes, format='PNG')
+        img_new.save(bytes, format='PNG')
         bytes.seek(0)
+        img_new.save("./tes.png")
         return send_file(bytes, mimetype='image/png')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
